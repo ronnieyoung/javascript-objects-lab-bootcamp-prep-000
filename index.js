0b86fb542db6f7aa9cb45b4449f589c96042a188
Ronald Young <ronnie.kingsley.young@gmail.com>
2017-12-04 01:44:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ object [key] = value;
 return object;
 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign ({},object,{[key], value})
+  return Object.assign ({},object,{[key], value});
 }
